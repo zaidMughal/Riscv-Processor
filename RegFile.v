521fa565fa2825e89aa6d45e1990c9399dbdadf9
Zaid Mughal <51900989+zaidMughal@users.noreply.github.com>
2020-04-07 11:53:33 +0500

    Update RegFile.v

--- RegFile.v
+++ RegFile.v
@@ -2,7 +2,7 @@ module registmem(
 						input [4:0]rd,r1,r2,
 						input [31:0]dataIn,
 						input RegWEn,clk,AReset,
-						output [31:0]data1,data2,DataReg11
+						output [31:0]data1,data2,DataReg5,DataReg11,DataReg13
 						);
 parameter p=32;
 
@@ -13,24 +13,20 @@ assign wreg = {wreg_temp[31:1],1'b0};
 
 genvar i;
 generate for(i = 0; i < p; i = i + 1)begin:regfile
-			register r(dataIn,clk,AReset,wreg[i],regout[i]);
+			if(i!=2)
+			register #(.W(p),.d(0)) r(dataIn,clk,AReset,wreg[i],regout[i]);
+			else
+			register #(.W(p),.d(31)) r(dataIn,clk,AReset,wreg[i],regout[i]);//31 is max stack size
 end
 endgenerate
 
 assign data1 = regout[r1];
 assign data2 = regout[r2];
+assign DataReg5 = regout[17];
 assign DataReg11 = regout[27];
+assign DataReg13 = regout[13];
 endmodule
-/*
-module MUX32b2in(
-				input [Bits-1:0]data[2**selLines-1:0],
-				input [selLines-1:0]sel,
-				output [Bits-1:0]dataout
-				);
-parameter selLines=1;
-parameter Bits=32;
-assign dataout = data[sel]
-*/
+
 module decoder(
 					input [4:0]dec_in,
 					input enable,
@@ -44,14 +40,14 @@ module register(
 					input clock_signal,reset,we,
 					output reg [W-1:0]register_variable
 					);
-parameter W = 32;
+parameter W = 32,d = 0;
 initial register_variable = 0;
 always @ (negedge reset or posedge clock_signal)
 begin
 	// Reset whenever the reset signal goes low, regardless of the clock
 	if (!reset)
 		begin
-			register_variable <= 0;
+			register_variable <= d;
 		end
 	else if(we)
 		begin
@@ -64,7 +60,7 @@ begin
 end
 endmodule
 
-module upcounter(input clk,rst,Pcsel,input [9:0]load,output [9:0]add);
+module upcounter(input clk,rst,Pcsel,input [13:0]load,output [13:0]add);
 initial add = 0;
 always @ (negedge rst or posedge clk)
 begin
