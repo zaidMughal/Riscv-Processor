eff2970e485177fdd6d62560404be964100dc485
Zaid Mughal <51900989+zaidMughal@users.noreply.github.com>
2020-04-07 11:47:36 +0500

    Update ALUFile.v

--- ALUFile.v
+++ ALUFile.v
@@ -1,39 +1,29 @@
 module ALU(
 				input [31:0]a,b,
-				input clk,Areset,
 				input [3:0]ALUSel,
 				output [31:0]result
 				);
-wire [31:0]addsb,addsb0,shft,a4,b4;
-wire [3:0]ALUSel4;
-wire LT;
-LPMaddersubtractorL t(~Areset,~ALUSel[2],clk,a,b,addsb);
-LPMaddersubtractorL t0(~Areset,1'b1,clk,32'b0,b,addsb0);
-comp4 f(~Areset,clk,a,b,LT);
-shift4 s(~Areset,clk,a,~ALUSel[1],b[4:0],shft);
-registerS #(32,2) ar(a,clk,Areset,1'b1,a4);
-registerS #(32,2) br(b,clk,Areset,1'b1,b4);
-registerS #(4,2) alr(ALUSel,clk,Areset,1'b1,ALUSel4);
-
-always@(a4,b4,addsb,shft,LT,ALUSel4,addsb0)
+//wire [31:0] r;
+//multip mmm(a,b,r);
+always@(*)
 begin
-case(ALUSel4)
-4'b0000:result = addsb;
-4'b0001:result = a4 & b4;
-4'b0010:result = a4 | b4;
-4'b0011:result = a4 ^ b4;
-4'b0100:result = shft;
-4'b0101:result = a4;//$signed(a) >>> b[4:0];
-4'b0110:result = shft;//a << b[4:0];
-4'b0111:result = {31'b0,LT};
-4'b1000:result =addsb0;// a / b;
-4'b1001:result =a4;// a % b;
-4'b1010:result =a4;// {{32'd0, a}*b} >> 32;
-4'b1011:result =a4;// (a * b);
-4'b1100:result = addsb;
-4'b1101:result = b4;
-4'b1110:result = b4;
-4'b1111:result = b4;
+case(ALUSel/*4*/)
+4'b0000:result = a+b;//addsb;
+4'b0001:result = a & b;//a4 & b4;
+4'b0010:result = a | b;//a4 | b4;
+4'b0011:result = a ^ b;//a4 ^ b4;
+4'b0100:result = a >> b[4:0];//shft;
+4'b0101:result = a;//a4;//$signed(a) >>> b[4:0];
+4'b0110:result = a << b[4:0];//shft;
+4'b0111:result = {31'b0,a<b};
+4'b1000:result =b;//for lui(send b as it is)
+4'b1001:result =a;// a % b;
+4'b1010:result =a;// {{32'd0, a}*b} >> 32;
+4'b1011:result = a* b;// (a * b) for mul
+4'b1100:result = a - b;
+4'b1101:result = b;
+4'b1110:result = b;
+4'b1111:result = b;
 endcase
 end
 endmodule
@@ -70,7 +60,7 @@ initial Count=1'b0;
 
 always@(posedge clk)
 	begin
-	if(Num==26'b10111110101111000010000)
+	if(Num==26'b1111000010000)
 		begin
 		Count<=~Count;
 		Num<=26'b0;
